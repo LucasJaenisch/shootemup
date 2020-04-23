8c2373e8315e9ff19ef7dd963ec421adf4773edf
lucasjaenisch <lucasirmaododaniel@gmail.com>
2020-04-23 18:33:02 -0300

    Shotgun now works (graficly),

--- shootemup/Scenes/Weapon_Shotgun.gd
+++ shootemup/Scenes/Weapon_Shotgun.gd
@@ -1,42 +1,78 @@
 extends Position2D
 
-# Variables for storing how much ammo is in the weapon, how much spare ammo this weapon has
-# and how much ammo is in a full weapon/magazine.
 var ammo_in_weapon = 10
 var spare_ammo = 20
+
+var bullet_origin = Vector2()
+
 const AMMO_IN_MAG = 10
 
 const DAMAGE = 15
 
-# Can this weapon reload?
 const CAN_RELOAD = true
-# Can this weapon be refilled
 const CAN_REFILL = true
 
+const GUN_OFFSET_X = Vector2(17,5)
+const GUN_OFFSET_Y = Vector2(0,20)
 
 var bullet_scene = preload("Bullet.tscn")
-
-# The player script. This is so we can easily access the animation player
-# and other variables.
 var player_node = null
 
+var rate_of_fire
+
 func _ready():
-	# We are going to assume the player will pass themselves in.
-	# While we can have cases where the player does not pass themselves in,
-	# having a complicated get_node call does not look pretty and it (relatively) safe to assume
-	# player_node will be passed in.
 	pass
 	
-
 func fire_weapon(direction):
-	var clone = bullet_scene.instance()
+	var clone0 = bullet_scene.instance()
+	var clone1 = bullet_scene.instance()
+	var clone2 = bullet_scene.instance()
 	var scene_root = get_tree().root.get_children()[0]
-	scene_root.add_child(clone)
+	scene_root.add_child(clone0)
+	scene_root.add_child(clone1)
+	scene_root.add_child(clone2)
 	
-	clone.transform = Transform2D(Vector2(direction.x,direction.y), Vector2(0,0), position)
-	clone.scale = Vector2(1, 1)
-	clone.BULLET_DAMAGE = DAMAGE
+	clone0.BULLET_DAMAGE = DAMAGE
+	clone1.BULLET_DAMAGE = DAMAGE
+	clone2.BULLET_DAMAGE = DAMAGE
+	
+	clone0.KILL_TIMER = 0.2
+	clone1.KILL_TIMER = 0.2
+	clone2.KILL_TIMER = 0.2
+
+	bullet_origin = get_parent().get_parent().get_parent().position
+	
+	if direction.x != 0:
+		if direction.x > 0:
+			bullet_origin.x = get_parent().get_parent().get_parent().position.x + GUN_OFFSET_X.x
+			bullet_origin.y = get_parent().get_parent().get_parent().position.y - GUN_OFFSET_X.y
+		else:
+			bullet_origin.x = get_parent().get_parent().get_parent().position.x - GUN_OFFSET_X.x
+			bullet_origin.y = get_parent().get_parent().get_parent().position.y - GUN_OFFSET_X.y
+	elif direction.y != 0:
+		if direction.y > 0:
+			bullet_origin.y = get_parent().get_parent().get_parent().position.y
+		else:
+			bullet_origin.y = get_parent().get_parent().get_parent().position.y - GUN_OFFSET_Y.y
+	
+	# First vector is the bullet direction
+	# Second vector ???
+	# Third vector is the bullet spawn origin
+	
+	if(direction.y != 0):
+		clone0.transform = Transform2D(Vector2(direction.x,direction.y), Vector2(0,0), bullet_origin)
+		clone0.scale = Vector2(1, 1)
+		clone1.transform = Transform2D(Vector2(0.25,direction.y), Vector2(0,0), bullet_origin)
+		clone1.scale = Vector2(1, 1)
+		clone2.transform = Transform2D(Vector2(-0.25,direction.y), Vector2(0,0), bullet_origin)
+		clone2.scale = Vector2(1, 1)
+	else:
+		clone0.transform = Transform2D(Vector2(direction.x,direction.y), Vector2(0,0), bullet_origin)
+		clone0.scale = Vector2(1, 1)
+		clone1.transform = Transform2D(Vector2(direction.x,0.25), Vector2(0,0), bullet_origin)
+		clone1.scale = Vector2(1, 1)
+		clone2.transform = Transform2D(Vector2(direction.x,-0.25), Vector2(0,0), bullet_origin)
+		clone2.scale = Vector2(1, 1)
 	ammo_in_weapon -= 1
 	
-	# Play the gun sound
 	$Shotgun_Sound.play()
